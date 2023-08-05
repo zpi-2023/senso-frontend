e300b4e20f4184702b60cfed7167cef415fa8374
Tomasz Chojnacki <tomaszchojnacki2001@gmail.com>
2023-08-05 15:33:50 +0200

    PR:  Setup linting, formatting and typechecking on frontend #4  (#7)

--- metro.config.js
+++ metro.config.js
@@ -1,5 +1,7 @@
+/* eslint-env node */
+
 // Learn more https://docs.expo.io/guides/customizing-metro
-const { getDefaultConfig } = require('expo/metro-config');
+const { getDefaultConfig } = require("expo/metro-config");
 
 /** @type {import('expo/metro-config').MetroConfig} */
 const config = getDefaultConfig(__dirname, {
