af18b26455e0d21f6c6210e1cab33b2ba8963cd7
Tomasz Chojnacki <tomaszchojnacki2001@gmail.com>
2023-08-27 20:34:54 +0200

    Support conditional fetching

--- util/api/swr.ts
+++ util/api/swr.ts
@@ -9,16 +9,22 @@ const TEST_CONFIG = {
   dedupingInterval: 0,
 } as const;
 
-type UseApiArg<P extends GetPath> = { url: P } & Pick<GetOptions<P>, "params">;
+type UseApiArg<P extends GetPath> =
+  | ({ url: P } & Pick<GetOptions<P>, "params">)
+  | null;
 
-export const useApi = <P extends GetPath>({ url, params }: UseApiArg<P>) => {
+const buildOptions = <P extends GetPath>(
+  { params }: NonNullable<UseApiArg<P>>,
+  token: string | null,
+): GetOptions<P> => ({
+  ...({ params } as GetOptions<P>),
+  headers: { Authorization: `Bearer ${token}` },
+});
+
+export const useApi = <P extends GetPath>(arg: UseApiArg<P>) => {
   const { token } = useAuth();
-  const options = {
-    ...({ params } as GetOptions<P>),
-    headers: { Authorization: `Bearer ${token}` },
-  };
   return useSWR(
-    [url, options],
+    arg ? [arg.url, buildOptions(arg, token)] : null,
     (args) => get(...args),
     __DEV__ ? TEST_CONFIG : undefined,
   );
