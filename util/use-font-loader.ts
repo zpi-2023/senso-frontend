3ab77bdafd672149268aa56b82c8b219409dbd60
Tomasz Chojnacki <tomaszchojnacki2001@gmail.com>
2023-08-29 23:04:30 +0200

    Setup routing #12  (#19)
    Answering questions from the issue:
    - What are our options for routing? - [Expo
    Router](https://docs.expo.dev/routing/introduction/)
    - Is one of them preferred/recommended/default? - yes, Expo Router
    - How to handle passing arguments between routes? - [Use Search
    Parameters](https://docs.expo.dev/router/reference/search-parameters/)
    - Are routes with parameters allowed? - yes, [Dynamic
    Routes](https://docs.expo.dev/routing/create-pages/#dynamic-routes)
    
    You should probably read first few pages of the [Expo Router
    Docs](https://docs.expo.dev/routing/introduction/).
    
    Other than that, I've cleaned up the boilerplate left by the default
    project template.

---
+++ util/use-font-loader.ts
@@ -0,0 +1,23 @@
+import { FontAwesome } from "@expo/vector-icons";
+import { useFonts } from "expo-font";
+import { SplashScreen } from "expo-router";
+import { useEffect } from "react";
+
+export const useFontLoader = () => {
+  const [loaded, error] = useFonts({
+    SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
+    ...FontAwesome.font,
+  });
+
+  useEffect(() => {
+    if (error) throw error;
+  }, [error]);
+
+  useEffect(() => {
+    if (loaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [loaded]);
+
+  return loaded;
+};
