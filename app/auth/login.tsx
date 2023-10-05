11c26a562d405211a148783b16c28337e738969f
Jakub Czerwiński <kubaczerwinski77@gmail.com>
2023-10-05 23:36:17 +0200

    Home screen placeholder

---
+++ app/auth/login.tsx
@@ -0,0 +1,24 @@
+import { Link } from "expo-router";
+import { StyleSheet, View } from "react-native";
+import { Button, Text } from "react-native-paper";
+
+const Page = () => {
+  return (
+    <View style={styles.container}>
+      <Text>Login page</Text>
+      <Link href="/auth/register" replace>
+        <Button>Go to register</Button>
+      </Link>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
+
+export default Page;
