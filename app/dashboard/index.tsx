9d36c169d990a4d3d465bd1743be068053ff1ef2
Tomasz Chojnacki <tomaszchojnacki2001@gmail.com>
2023-10-26 21:36:02 +0200

    Fetch dashboard gadgets dynamically

--- app/dashboard/index.tsx
+++ app/dashboard/index.tsx
@@ -1,6 +1,8 @@
 import { FlatList, StyleSheet } from "react-native";
+import { ActivityIndicator } from "react-native-paper";
 
 import { actions, ActionKey } from "@/common/actions";
+import { useApi } from "@/common/api";
 import { useI18n } from "@/common/i18n";
 import { useIdentity, RedirectIfNoProfile } from "@/common/identity";
 import { DashboardGadget } from "@/components/DashboardGadget";
@@ -8,28 +10,38 @@ import { Header } from "@/components/Header";
 import { SosFab } from "@/components/SosFab";
 import { View } from "@/components/Themed";
 
-const mockDashboardGadgets: ActionKey[] = [
-  "trackMedication",
-  "manageNotes",
-  "playGames",
-  "pairCaretaker",
-  "switchProfile",
-  "logOut",
-];
-
 const Page = () => {
   const { t } = useI18n();
   const identity = useIdentity();
+  const { data } = useApi(
+    identity.hasProfile
+      ? {
+          url: "/api/v1/dashboard/{seniorId}",
+          params: { path: { seniorId: identity.profile.seniorId } },
+        }
+      : null,
+  );
 
   if (!identity.hasProfile) {
     return <RedirectIfNoProfile identity={identity} />;
   }
 
+  if (!data?.gadgets) {
+    return (
+      <View style={styles.loadingContainer}>
+        <Header title={t("dashboard.pageTitle")} />
+        <ActivityIndicator size="large" />
+      </View>
+    );
+  }
+
+  const gadgets = data.gadgets as ActionKey[];
+
   return (
     <View style={styles.container}>
       <Header left={actions.openMenu} title={t("dashboard.pageTitle")} />
       <FlatList
-        data={mockDashboardGadgets}
+        data={gadgets}
         numColumns={2}
         renderItem={({ item }) => <DashboardGadget action={actions[item]} />}
         style={styles.list}
@@ -45,6 +57,11 @@ const styles = StyleSheet.create({
   },
   list: {
     padding: 8,
+  },
+  loadingContainer: {
+    flex: 1,
+    display: "flex",
+    justifyContent: "center",
   },
 });
 
