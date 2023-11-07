ef0faa09abcf9d2378588da81bc574ac9f04a74f
Jakub Czerwiński <kubaczerwinski77@gmail.com>
2023-11-08 00:28:07 +0100

    Change error color and i18n

--- components/SosFab.tsx
+++ components/SosFab.tsx
@@ -1,16 +1,26 @@
 import { useState } from "react";
 import { StyleSheet } from "react-native";
-import { FAB, Modal, Portal, Text } from "react-native-paper";
+import {
+  FAB,
+  type MD3Theme,
+  Modal,
+  Portal,
+  Text,
+  useTheme,
+} from "react-native-paper";
 
 import SosSlider from "./SosSlider";
 
 import { actions, useActionContext } from "@/common/actions";
+import { colors } from "@/common/constants";
 import { useI18n } from "@/common/i18n";
 
 const action = actions.activateSos;
 
 export const SosFab = () => {
   const { t } = useI18n();
+  const theme = useTheme();
+  const styles = makeStyles(theme);
   const [visible, setVisible] = useState(false);
   const ctx = useActionContext();
 
@@ -33,16 +43,10 @@ export const SosFab = () => {
         <Modal
           visible={visible}
           onDismiss={handleClose}
-          contentContainerStyle={{
-            padding: 30,
-            margin: 20,
-            borderRadius: 10,
-            height: 200,
-            backgroundColor: "green",
-          }}
+          contentContainerStyle={styles.contentContainerStyle}
         >
           <Text variant="headlineLarge" style={{ textAlign: "center" }}>
-            Slide to call for help
+            {t("sosSlider.description")}
           </Text>
           <SosSlider />
         </Modal>
@@ -51,11 +55,19 @@ export const SosFab = () => {
   );
 };
 
-const styles = StyleSheet.create({
-  fab: {
-    position: "absolute",
-    bottom: 32,
-    right: 32,
-    backgroundColor: "#991b1b",
-  },
-});
+const makeStyles = (theme: MD3Theme) =>
+  StyleSheet.create({
+    contentContainerStyle: {
+      padding: 30,
+      margin: 20,
+      borderRadius: 10,
+      height: 200,
+      backgroundColor: theme.colors.background,
+    },
+    fab: {
+      position: "absolute",
+      bottom: 32,
+      right: 32,
+      backgroundColor: colors.error,
+    },
+  });
