f78b64702e4ea35613ec73cfe3d16a259d37da9b
Jakub Czerwiński <kubaczerwinski77@gmail.com>
2023-12-04 15:19:29 +0100

    Increase empty cells amount

--- logic/sudoku.ts
+++ logic/sudoku.ts
@@ -1,5 +1,5 @@
 // Cells to fill in the board
-export const emptyCellCount = 20;
+export const emptyCellCount = 30;
 
 // Function to check if a value can be placed in a given cell
 const isValid = (
